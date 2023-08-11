225d4772ee5d3f5660c10ff0c2fd16ab766508c4
Aman Singh <amansinghs7010@gmail.com>
2023-08-11 12:16:12 -0400

    setup tests and create tests for new route

---
+++ leaderboard/src/routes/__tests__/new.test.ts
@@ -0,0 +1,24 @@
+import request from 'supertest';
+
+import { app } from '../../app';
+import { Player } from '../../models/player';
+
+it('has a route handler listening to /api/leaderboard for post requests', async () => {
+  const response = await request(app).post('/api/leaderboard').send({});
+
+  expect(response.status).not.toEqual(404);
+});
+
+it('creates a player with valid inputs', async () => {
+  const name = 'Player Name';
+  const score = 500;
+
+  await request(app).post('/api/leaderboard').send({ name, score }).expect(201);
+
+  const players = await Player.find({});
+  expect(players.length).toEqual(1);
+  expect(players[0].name).toEqual(name);
+  expect(players[0].score).toEqual(score);
+});
+
+it.todo('returns an error if invalid name or price is provided');
