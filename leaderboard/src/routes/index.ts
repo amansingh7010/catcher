452e027d0cd7668211dc8d20e090de9e553cfe69
Aman Singh <amansinghs7010@gmail.com>
2023-08-12 18:45:24 -0400

    add limit to the fetch leaderboard query

--- leaderboard/src/routes/index.ts
+++ leaderboard/src/routes/index.ts
@@ -4,8 +4,9 @@ import { Player } from '../models/player';
 
 const router = express.Router();
 
+// Fetch 100 records sorted by score in descending order
 router.get('/api/leaderboard', async (req: Request, res: Response) => {
-  const players = await Player.find({}).sort('-score');
+  const players = await Player.find({}).sort('-score').limit(100);
   res.send(players);
 });
 
